3abca979dca0c5c8f4b13906b1ee65533668b5e2
Ahkar02468 <arkarshwebaw@gmail.com>
2023-03-30 21:42:06 +0630

    slove first 3 problems

--- problems/02-values-in-object.js
+++ problems/02-values-in-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 
 Do this once using using a `for...in` loop and once using `Object.values`.
@@ -16,7 +16,28 @@ valuesInObject(foods); // => ["tart", "sour", "sweet"]
 
 function valuesInObject(obj) {
   // Your code here
+  let arr = [];
+  for(let keys in obj){
+    arr.push(obj[keys]);
+
+  }
+  return arr;
 }
 
+
+// let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
+// let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
+const animalValue = valuesInObject({
+  dog: "Wolfie",
+  cat: "Jet",
+  bison: "Bilbo"
+});
+console.log(animalValue);// => ["Wolfie", "Jet", "Bilbo"]
+const foodValue = valuesInObject({
+  apple: "tart",
+  lemon: "sour",
+  mango: "sweet"
+});
+console.log(foodValue); // => ["tart", "sour", "sweet"]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuesInObject;
--- problems/03-set-key-in-object.js
+++ problems/03-set-key-in-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function `setKeyInObject(obj, string, value)` that takes in three 
-parameters. The first parameter is an object, the second parameter will be a 
+Write a function `setKeyInObject(obj, string, value)` that takes in three
+parameters. The first parameter is an object, the second parameter will be a
 string and the third parameter will be a value. Your job is to return the object
 adding the second parameter as a key using the third parameter as its value.
 
@@ -15,7 +15,22 @@ setKeyInObject(obj1, "num", 3); // => {str: "hello", num: 3}
 
 function setKeyInObject(obj, string, value) {
   // Your code here
+  // let keyAndObject = {};
+  // // if(obj === null){
+  // //   keyAndObject = {string, value};
+  // // }
+  let keyAndObject = ""
+  for(let key in obj){
+    let keys = key;
+    console.log(keys);
+    keyAndObject = { keys:obj[key], string: value}
+  }
+  // const keyAndObject = {obj.key:obj.value, string:value};
+  return console.log(keyAndObject);
 }
+
+let obj = {str: "hello"}
+setKeyInObject(obj, "num", "3"); // => {apple: "yum"}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = setKeyInObject;
