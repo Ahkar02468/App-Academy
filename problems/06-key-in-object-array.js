85106c911aba16d2baa4201a354d3b5f381ef3f9
Ahkar02468 <arkarshwebaw@gmail.com>
2023-04-04 22:16:55 +0630

    solve prob- 6

--- problems/06-key-in-object-array.js
+++ problems/06-key-in-object-array.js
@@ -1,7 +1,7 @@
 /***********************************************************************
-Write a function `keyInObjectArray(objArray, keyString)` that takes in an array of 
-objects as the first parameter and a string as the second. The `keyInObjectArray` 
-will return `true` if any of the objects contains the `keyString` as a key within them, and 
+Write a function `keyInObjectArray(objArray, keyString)` that takes in an array of
+objects as the first parameter and a string as the second. The `keyInObjectArray`
+will return `true` if any of the objects contains the `keyString` as a key within them, and
 `false` if not.
 
 
@@ -21,7 +21,22 @@ keyInObjectArray(objArray, 'animal'); // => false
 
 function keyInObjectArray(objArray, keyString) {
   // Your code here
-}
+  // Loop through each object in the array
+  for (let i = 0; i < objArray.length; i++) {
+    // Check if the object contains the keyString as a key
+    if (objArray[i].hasOwnProperty(keyString)) {
+      return true; // Return true if keyString is found in any of the objects
+    }
+  }
+  return false; // Return false if keyString is not found in any of the objects
+  }
+
+let objArray = [
+  { name: "Rupert" },
+  { age: 42 },
+  { planet: "Earth", system: "Milky Way" }
+];
+console.log(keyInObjectArray(objArray, 'plat'));
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyInObjectArray;
