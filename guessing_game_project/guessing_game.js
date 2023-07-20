863c7343a299e4450d8b8b6b8c244af67607bf38
aksb <Ahkar02468>
2023-07-20 22:34:44 +0630

    slove guessing game

--- guessing_game_project/guessing_game.js
+++ guessing_game_project/guessing_game.js
@@ -3,35 +3,64 @@ const { stdin: input, stdout: output } = require('node:process');
 
 const rl = readline.createInterface({ input, output });
 
-let secretNumber = 34;
+let secretNumber = 0;
+let numAttempts = 0;
 
 const checkGuess = (checkNumber) => {
     checkNumber = Number(checkNumber);
     if(checkNumber > secretNumber){
         console.log("Too High");
-        rl.close();
         return false;
         
     }
     if(checkNumber < secretNumber){
         console.log("Too Low");
-        rl.close();
         return false;
     }
-    if(checkNumber === secretNumber){
-        console.log("Correct");
-        rl.close();
-        return true;
-    }
+    console.log("Correct");
+    return true;
     
 }
 
-function askGuess() {
-    return rl.question('Enter a guess: ', checkGuess);
+const askGuess = () => {
+   rl.question('Enter a guess: ', (answer) => {
+    
+    if(numAttempts === 0){
+        console.log('You lose you have ' + numAttempts + ' attempt left.')
+        rl.close();
+        return false;
+    }
+    if(checkGuess(answer)){
+        console.log("You win!");
+        rl.close();
+    }else{
+        numAttempts--;
+        askGuess();  
+    }
+    
+   });
 }
 
-askGuess();
+const randomInRange = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
 
-// askGuess.question('Enter a guess: ', checkGuess);
+const askRange = () => {
+    rl.question("Enter a min number: ", (min) => {
+      rl.question("Enter a max number: ", (max) => {
+        secretNumber = randomInRange(min, max);
+        askGuess();
+      });
+    });
+  };
 
-// console.log(checkGuess(34));
+const asklimit = () => {
+    rl.question('Enter attemps you want to try: ', (attempts) => {
+        numAttempts = Number(attempts);
+        askRange();
+    })
+}
+
+asklimit();
