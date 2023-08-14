37997c7145fa7d893131161c628f0e0d4e8eab4d
aksb <Ahkar02468>
2023-08-14 23:45:03 +0630

    solve dragon class problems

--- dragons-long-practice-class/classes/dragon.js
+++ dragons-long-practice-class/classes/dragon.js
@@ -4,7 +4,24 @@ class Dragon{
     this.name = name;
     this.color = color;
   }
+
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons){
+    let dragonsArr = []
+    dragons.forEach(dragon => {
+      dragonsArr.push(dragon.name);
+    })
+    return dragonsArr;
+  }
 }
+
+// const dragon1 = new Dragon('Puff','black');
+// const dragon2 = new Dragon('withtooth','green');
+// const dragons = Dragon.getDragons(dragon1, dragon2);
+// console.log(dragons); 
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
